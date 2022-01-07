d8b713975d7b37cacb8a9451aaf0a51d7b72afe2
Sarah Alexander <sjalex78@gmail.com>
2022-01-07 17:06:09 +1100

    Completing highscore array practice

---
+++ ruby/high-scores/high_scores.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class HighScores
+  attr_reader :scores
+
+  def initialize(scores)
+    @scores = scores
+  end
+
+  def latest
+    @scores.last
+  end
+
+  def personal_best
+    @scores.max
+  end
+
+  def personal_top_three
+    @scores.sort.reverse.slice(0, 3)
+  end
+
+  def latest_is_personal_best?
+    @scores[-1] == personal_best
+  end
+end
